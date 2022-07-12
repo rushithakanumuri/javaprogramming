b3141099199f3dd4bd8072692b25708417820b3b
rushithakanumuri <99245826+rushithakanumuri@users.noreply.github.com>
2022-07-13 03:24:20 +0530

    Add files via upload

---
+++ SimplifiedIf.java
@@ -0,0 +1,12 @@
+
+public class SimplifiedIf {
+    public static void main(String[] args){
+        int income = 120_000;
+        boolean hasHighIncome =  (income>100_000);
+
+        System.out.println(hasHighIncome);
+        
+    }       
+    
+}
+

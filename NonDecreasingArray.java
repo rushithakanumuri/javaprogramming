b5737d6baabbf5aa9452fe7820b0fd3795334a98
rushithakanumuri <99245826+rushithakanumuri@users.noreply.github.com>
2022-07-18 11:53:07 +0530

    Update NonDecreasingArray.java

--- NonDecreasingArray.java
+++ NonDecreasingArray.java
@@ -4,12 +4,26 @@ class NonDecreasingArray {
         int nums[] = new int[3];
         nums[0] = 10;
         nums[1] = 5;
-        nums[2] = 7;
-        int count = 0;
+        nums[2] = 1;
+
+        if(isNonDecreasingArray(nums)){
+            System.out.println("true");
+
+        }else{
+            System.out.println("false");
+        }
+             
+    }
+        
+    
+     private static boolean isNonDecreasingArray(int[] nums){
+           
+            int count = 0;    
+        boolean isNonDecreasingArray = true;
         for(int i=0;i<nums.length-1 ;i++){
             if(nums[i]>nums[i+1]){
                 if(count == 1){
-                    System.out.println("false");
+                    return false;
                 }
                 count++;
                 if(i>0 && nums[i-1]<=nums[i+1])
@@ -17,10 +31,10 @@ class NonDecreasingArray {
                 else
                     nums[i]=nums[i+1];
 
-                
+          
             }       
         }
         
-        System.out.println("true");
+        return true;
     }
 }
